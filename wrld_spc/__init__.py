4792baedb7b5e0ebf6e743986a58e77c72ac732c
Jeremiah Shelor <jshel012@odu.edu>
2023-01-30 20:56:47 -0500

    set up directories, added gitignore

---
+++ wrld_spc/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRETE_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'src.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+def hello_world():
+    return 'Hello, World!'
