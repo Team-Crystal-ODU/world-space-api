3a69b0f9ca19b326b43e0334aab56a2630eded1b
Jeremiah Shelor <jshel012@odu.edu>
2023-04-17 10:04:37 -0400

    added basis auth check endpoint

--- world_spc/resources/auth.py
+++ world_spc/resources/auth.py
@@ -4,9 +4,36 @@ from marshmallow import Schema, fields
 from world_spc.extensions import mongo
 
 
+class AuthSchema(Schema):
+    user = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+
+def check_user_and_password(db, user, password):
+    col = db['auth']
+    if col.count_documents({"user": user}) > 0:
+        id = col.find_one({"user": user})["_id"]
+        if col.find_one({"_id": id})['password'] == password:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+schema = AuthSchema()
+
+
 class Auth(Resource):
     def get(self):
-        pass
+        errors = schema.validate(request.form)
+        if errors:
+            abort(400, str(errors))
 
-    def post(self):
-        pass
+        user = request.form['user']
+        password = request.form['password']
+
+        if check_user_and_password(mongo.db, user, password):
+            return "Success."
+        else:
+            abort(400, "User and password combination not found.")
